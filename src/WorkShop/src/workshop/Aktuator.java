012307938929bb99373fbc4cdbb53b7d44225f0d
Lise Olsen <liseolsen@Lises-MacBook-Pro.local>
2019-11-20 12:49:14 +0100

    Aktuator Class

---
+++ src/WorkShop/src/workshop/Aktuator.java
@@ -0,0 +1,17 @@
+package workshop;
+
+public class Aktuator {
+    private int value;  
+
+    public Aktuator(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
