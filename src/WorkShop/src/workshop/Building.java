8a84b86673afc23a253311738c4733dd8dcec459
Lise Olsen <liseolsen@Lises-MacBook-Pro.local>
2019-11-20 13:13:27 +0100

    Start på Building class

---
+++ src/WorkShop/src/workshop/Building.java
@@ -0,0 +1,28 @@
+package workshop;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author liseolsen
+ */
+public class Building {
+   private String name;
+   private ArrayList<Sensor> sensors = new ArrayList<>();
+   private Aktuator[] aktuator;
+
+    public String getName() {
+        return name;
+    }
+
+    public Sensor[] getSensor() {
+        return sensors;
+    }
+
+    public Aktuator[] getAktuator() {
+        return aktuator;
+    }
+    
+    public void addSensor(Sensor);
+         sensor.add
+}
